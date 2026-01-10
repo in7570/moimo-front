e6184b7f84b140fd5c7c90c73c0cf8e7b7fd30e0
KimHyeonSu <hirobin2001@gmail.com>
2026-01-10 20:47:03 +0900

    feat: MeetingList 컴포넌트 구현

---
+++ src/components/common/MeetingList.tsx
@@ -0,0 +1,20 @@
+import MeetingCard from "@/components/common/MeetingCard";
+import type { Meeting } from "@/models/meeting.model";
+
+interface MeetingListProps {
+  meetings: Meeting[];
+}
+
+const MeetingList = ({ meetings }: MeetingListProps) => {
+  return (
+    <div className="w-full max-w-6xl mx-auto py-8">
+      <div className="grid grid-cols-4 gap-4 justify-items-center">
+        {meetings.map((meeting) => (
+          <MeetingCard key={meeting.meetingId} meeting={meeting} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default MeetingList;
