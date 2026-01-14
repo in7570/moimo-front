10a8ae944ba90133d25627ec7495ca93b40177d6
kyj <7saval@gmail.com>
2026-01-14 12:19:13 +0900

    fix: 본문과 푸터 사이의 레이아웃 간격 수정

--- src/pages/mypage/MypageSession.tsx
+++ src/pages/mypage/MypageSession.tsx
@@ -3,9 +3,9 @@ import { Outlet } from "react-router-dom";
 
 const MypageSession = () => {
     return (
-        <div className="flex w-full h-full bg-background">
+        <div className="flex w-full flex-1 bg-background">
             {/* leftSidebar - 마이페이지바 */}
-            <div className="flex-[3] overflow-hidden">
+            <div className="flex-[3] flex flex-col overflow-hidden">
                 <MypageSidebar />
             </div>
             {/* 메인 영역 - 스크롤 가능하도록 설정 */}
