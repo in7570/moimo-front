ed29a8674795e41fc03cdaf3370506317ad16428
in7570 <i7570@naver.com>
2026-01-19 04:52:22 +0900

    design: 헤더, 푸터 좌우 간격+ 디자인 수정

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -7,50 +7,52 @@ import { Link } from "react-router-dom";
 function Header() {
   const { isLoggedIn } = useAuthStore();
   return (
-    <div className="flex items-center w-full h-[67px] bg-card sticky top-0 z-50 shrink-0">
-      <Button
-        asChild
-        size="lg"
-        variant="ghost"
-        className="cursor-pointer hover:bg-transparent font-bold text-xl"
-      >
-        <Link to="/">MoiMo</Link>
-      </Button>
-      <div className="flex gap-2 ml-8">
+    <div className="w-full h-[80px] bg-card sticky top-0 z-50 shrink-0 border-b border-gray-300">
+      <div className="flex items-center w-full h-full max-w-screen-xl mx-auto px-4 md:px-8">
         <Button
           asChild
-          size="default"
+          size="lg"
           variant="ghost"
-          className="cursor-pointer hover:bg-transparent"
+          className="cursor-pointer hover:bg-transparent font-bold text-2xl p-0"
         >
-          <Link to="/moimer-intro">모이머란?</Link>
+          <Link to="/">MoiMo</Link>
         </Button>
-        <Button
-          asChild
-          size="default"
-          variant="ghost"
-          className="cursor-pointer hover:bg-transparent"
-        >
-          <Link to="/meetings">원하는 모임 찾기</Link>
-        </Button>
-      </div>
-      {isLoggedIn ? (
-        <div className="ml-auto flex items-center gap-8 pr-4">
-          <NotificationDropdown />
-          <ProfileDropdown />
-        </div>
-      ) : (
-        <div className="login ml-auto pr-4">
+        <div className="flex gap-2 ml-8">
           <Button
             asChild
             size="default"
             variant="ghost"
-            className="cursor-pointer hover:bg-transparent text-sm"
+            className="cursor-pointer hover:bg-transparent text-base"
           >
-            <Link to="/login">로그인</Link>
+            <Link to="/moimer-intro">모이머란?</Link>
+          </Button>
+          <Button
+            asChild
+            size="default"
+            variant="ghost"
+            className="cursor-pointer hover:bg-transparent text-base"
+          >
+            <Link to="/meetings">원하는 모임 찾기</Link>
           </Button>
         </div>
-      )}
+        {isLoggedIn ? (
+          <div className="ml-auto flex items-center gap-4 md:gap-8">
+            <NotificationDropdown />
+            <ProfileDropdown />
+          </div>
+        ) : (
+          <div className="login ml-auto">
+            <Button
+              asChild
+              size="default"
+              variant="ghost"
+              className="cursor-pointer hover:bg-transparent text-base"
+            >
+              <Link to="/login">로그인</Link>
+            </Button>
+          </div>
+        )}
+      </div>
     </div>
   );
 }
